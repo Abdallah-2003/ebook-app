8d4081890e0f3e81797657c9b14f9773371ae9e0
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-11-28 16:48:53 +0200

    create structure of the project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const EbookApp());
+}
+
+
+class EbookApp extends StatelessWidget {
+  const EbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      
+    );
+  }
+}
