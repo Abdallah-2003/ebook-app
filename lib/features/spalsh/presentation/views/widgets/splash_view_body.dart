4a5598d70ea40a085e02dca51b53002bdd6558cb
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-12-02 15:26:46 +0200

    add animation to splash view

--- lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,14 +1,40 @@
 import 'package:ebook_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500)
+    );
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero).animate(animationController);
+    animationController.forward();
+  } 
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Center(
           child: ClipRRect(
@@ -20,6 +46,23 @@ class SplashViewBody extends StatelessWidget {
             ),
           ),
         ),
+        const SizedBox(
+          height: 10,
+        ),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            );
+          }
+        )
       ],
     );
   }
