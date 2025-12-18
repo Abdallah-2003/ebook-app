5daf2d1b35998c0da77139d9b4ab5d284f8d21a6
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-12-18 13:47:29 +0200

    regactor app navigation by using go router package instead get material app

--- lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,9 @@
-import 'package:ebook_app/constant.dart';
+import 'package:ebook_app/core/utils/app_router.dart';
 import 'package:ebook_app/core/utils/assets_data.dart';
-import 'package:ebook_app/features/home/presentation/views/home_view.dart';
 import 'package:ebook_app/features/spalsh/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:go_router/go_router.dart';
+
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -70,11 +69,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void navigateToHome() {
     Future.delayed(
       const Duration(seconds: 2),
-      () => Get.to(
-        () => const HomeView(),
-        transition: Transition.fade,
-        duration: kTransitionDuration,
-      ),
+      () {
+        if (!mounted) return;
+        context.go(AppRouter.kHomeView);
+        // GoRouter.of(context).push(AppRouter.kHomeView);
+      }
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:ebook_app/constant.dart';
-import 'package:ebook_app/features/spalsh/presentation/views/splash_view.dart';
+import 'package:ebook_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -14,12 +13,12 @@ class EbookApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor,
         textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
       ),
-      home: const SplashView(),
     );
   }
 }
