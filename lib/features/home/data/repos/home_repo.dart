93c07c11aeb95926225d2025babf3916b32288cb
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-12-28 15:36:26 +0200

    define methods type in home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:ebook_app/core/errors/failure.dart';
+import 'package:ebook_app/features/home/data/models/books_model/books_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchFeaturedBooks();
 }
