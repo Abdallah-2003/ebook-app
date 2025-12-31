34092ec60375a69bd972418d63cc1539a8327a59
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-12-31 14:27:31 +0200

    create newest books cubit

--- lib/features/home/presentation/view model/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/view model/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:ebook_app/features/home/data/models/books_model/books_model.dart';
+import 'package:ebook_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks () async {
+
+    emit(NewestBooksLoading());
+    final result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errMessage: failure.errMessage));
+      }, (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
 }
