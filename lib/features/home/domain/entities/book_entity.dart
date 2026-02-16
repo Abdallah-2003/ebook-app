93d85ddfb5d35d74b335dd161f7701eae1bb998e
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-16 11:41:44 +0200

    add domain layer and book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,12 @@
+class BookEntity {
+  final String image, title, author;
+  final num price, rating;
+
+  const BookEntity(
+    this.image,
+    this.title,
+    this.author,
+    this.price,
+    this.rating,
+  );
+}
