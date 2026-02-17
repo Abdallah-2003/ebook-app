176b9a9b2c8a6f4c8700dccc9e6b0118984b15cd
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-17 21:43:12 +0200

    register book entity adapter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:ebook_app/constant.dart';
 import 'package:ebook_app/core/utils/app_router.dart';
+import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 void main() {
+  Hive.registerAdapter(BookEntityAdapter());
+  Hive.openBox(kFeaturedBox);
   runApp(const EbookApp());
 }
 
