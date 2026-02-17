b04b39a5f58ecc324db76b40f6b9bb82b16f96de
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-17 22:45:17 +0200

    create home repo implementation

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -5,7 +5,4 @@ import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
   Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
-  Future<Either<Failure, List<BookEntity>>> fetchSimilarBooks({
-    required String category,
-  });
 }
