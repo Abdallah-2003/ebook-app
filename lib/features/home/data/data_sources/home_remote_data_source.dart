a333298323f7c9eed9872c8e5690b0c22cd21ef9
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-17 17:17:51 +0200

    create home remote data source

---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+
+class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    throw UnimplementedError();
+  }
+
+
+}
