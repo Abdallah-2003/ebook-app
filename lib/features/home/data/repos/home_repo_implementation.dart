857b05f23465ff52b2f8db908f514fab637a0651
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-17 23:06:07 +0200

    implement fetch featured books

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,13 +1,30 @@
 import 'package:dartz/dartz.dart';
 import 'package:ebook_app/core/errors/failure.dart';
+import 'package:ebook_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:ebook_app/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 import 'package:ebook_app/features/home/domain/repos/home_repo.dart';
 
-class HomeRepoImplementation extends HomeRepo{
+class HomeRepoImplementation extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImplementation({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      final booksList = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      final books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } on Exception catch (e) {
+      return left(Failure(errMessage: e.toString()));
+    }
   }
 
   @override
