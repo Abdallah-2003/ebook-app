c57c5e6153847b3bdc105f0c4025279c4f6946ad
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-02-17 21:37:04 +0200

    generate book entity adapter

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,12 +1,24 @@
+import 'package:hive_flutter/adapters.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
-  final String? image, title, author;
-  final num price, rating;
+  @HiveField(0)
+  final String? image;
+  @HiveField(1)
+  final String? title;
+  @HiveField(2)
+  final String? author;
+  @HiveField(3)
+  final num price;
+  @HiveField(4)
+  final num rating;
 
   const BookEntity({
     required this.image,
     required this.title,
     required this.author,
     required this.price,
-    required this.rating, 
-});
+    required this.rating,
+  });
 }
